bc9797cfb383708705199e3c3acfd59815e4f1b8
Igor Konobas <kois9911@gmail.com>
2018-10-17 19:42:12 +0300

    Refactored searching functions of the finder class

--- Homework_11_10_18/src/main/java/com/javacourse/VehicleFinder.java
+++ Homework_11_10_18/src/main/java/com/javacourse/VehicleFinder.java
@@ -5,29 +5,41 @@ import java.util.*;
 public class VehicleFinder {
 
     public static List<Vehicle> getPlanesWithHeightMoreThan5000YearAfter2000(List<Vehicle> vehicles){
-        final int YEAR_LIMIT = 2000;
-        final int HEIGHT_LIMIT = 5000;
-
         List<Vehicle> res = new ArrayList();
         for(var v:vehicles){
-            if(v instanceof Plane && v.getYearManufactured()>YEAR_LIMIT &&  ((Plane) v).getHeight()>HEIGHT_LIMIT){
+            if(isPlaneWithHeightMoreThan5000YearAfter2000(v)){
                 res.add(v);
             }
         }
         return res;
     }
 
-    public static List<Vehicle> getNotPlaneWithSpeedBetween200And500(List<Vehicle> vehicles){
-        final int SPEED_HIGH_LIMIT = 500;
-        final int SPEED_LOW_LIMIT = 200;
+    private static boolean isPlaneWithHeightMoreThan5000YearAfter2000(Vehicle v){
+        final int YEAR_LIMIT = 2000;
+        final int HEIGHT_LIMIT = 5000;
 
+        return (v instanceof Plane
+                && v.getYearManufactured()>YEAR_LIMIT
+                && ((Plane) v).getHeight()>HEIGHT_LIMIT);
+
+    }
+
+    public static List<Vehicle> getNotPlaneWithSpeedBetween200And500(List<Vehicle> vehicles){
         List<Vehicle> filteredVehicles = new ArrayList();
         for(var v:vehicles){
-            if(!(v instanceof Plane) && v.getSpeed()>SPEED_LOW_LIMIT && v.getSpeed()<SPEED_HIGH_LIMIT){
+            if(isNotPlaneWithSpeedBetween200And500(v)){
                 filteredVehicles.add(v);
             }
         }
         return filteredVehicles;
+    }
+
+    private static boolean isNotPlaneWithSpeedBetween200And500(Vehicle v){
+        final int SPEED_HIGH_LIMIT = 500;
+        final int SPEED_LOW_LIMIT = 200;
+        return !(v instanceof Plane)
+                && v.getSpeed()>SPEED_LOW_LIMIT
+                && v.getSpeed()<SPEED_HIGH_LIMIT;
     }
 
     public static FilteredResult getFilteredByWaysOfMOving(List<Vehicle> vehicles){
@@ -44,43 +56,49 @@ public class VehicleFinder {
     }
 
     public static List<Vehicle> getWithMaximalSpeed(List<Vehicle> vehicles){
-        int maximalSpeed = Collections
-                .max(vehicles, (v1, v2)->{return v1.getSpeed()-v2.getSpeed();})
+        int maximalSpeed = vehicles
+                .stream()
+                .max(Comparator.comparing(Vehicle::getSpeed))
+                .get()
                 .getSpeed();
-        List<Vehicle> res = new ArrayList();
+        List<Vehicle> res = new ArrayList<>();
         vehicles.forEach(x->{
             if(x.getSpeed()==maximalSpeed) res.add(x);
         });
         return  res;
     }
 
-    //TODO: implement it
     public static List<Vehicle> getWithMinPriceAndMaxSpeedYoungerThan5Years(List<Vehicle> vehicles){
-        final int AGE_LIMIT = 5;
-        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-
-        int minPrice = Collections
-                .min(vehicles, (Vehicle v1,Vehicle v2)->{return v1.getSpeed()-v2.getSpeed();})
+        int minPrice = vehicles
+                .stream()
+                .min(Comparator.comparing(Vehicle::getPrice))
+                .get()
                 .getPrice();
 
-        int maxSpeed = Collections
-                .max(vehicles, new Comparator<Vehicle>() {
-                    @Override
-                    public int compare(Vehicle v1, Vehicle v2) {
-                        return v1.getSpeed()-v2.getSpeed();
-                    }
-                }).getSpeed();
+        int maxSpeed = vehicles
+                .stream()
+                .max(Comparator.comparing(Vehicle::getSpeed))
+                .get()
+                .getSpeed();
 
-        List<Vehicle> res = new ArrayList();
+        List<Vehicle> res = new ArrayList<>();
         vehicles.forEach(x->{
-            if(x.getSpeed()==maxSpeed
-                && x.getPrice()==minPrice
-                && currentYear - x.getYearManufactured() < AGE_LIMIT){
+            if(hasMaxSpeedMinPriceAndYoungerThenWanted(x, maxSpeed, minPrice)){
                 res.add(x);
             }
         });
         return res;
     }
 
+    private static boolean hasMaxSpeedMinPriceAndYoungerThenWanted(Vehicle vehicle,
+                                                                   int maxSpeed,
+                                                                   int minPrice){
+        final int AGE_LIMIT = 5;
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+
+        return (vehicle.getSpeed()== maxSpeed
+                && vehicle.getPrice()== minPrice
+                && currentYear - vehicle.getYearManufactured() < AGE_LIMIT);
+    }
 
 }
