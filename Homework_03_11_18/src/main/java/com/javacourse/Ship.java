ab168fb341c80d97dfb9c939dabec89a428a2102
Igor Konobas <kois9911@gmail.com>
2018-11-03 18:50:50 +0200

    Added basic class sceleton without implementation

--- Homework_03_11_18/src/main/java/com/javacourse/Ship.java
+++ Homework_03_11_18/src/main/java/com/javacourse/Ship.java
@@ -1,4 +1,69 @@
 package com.javacourse;
 
-public class Ship {
+import java.util.Objects;
+
+public class Ship implements Runnable{
+
+    private int id;
+    private int totalCapacity;
+    private int currentCapacity;
+
+    private Ship(ShipBuilder builder) {
+        this.id = builder.id;
+        this.totalCapacity = builder.totalCapacity;
+        this.currentCapacity = builder.currentCapacity;
+    }
+
+    public static class ShipBuilder{
+        private int id;
+        private int totalCapacity;
+        private int currentCapacity;
+
+        public ShipBuilder(int id) {
+            this.id = id;
+        }
+
+        public ShipBuilder totalCapacity(int totalCapacity){
+            this.totalCapacity = totalCapacity;
+            return this;
+        }
+
+        public ShipBuilder currentCapacity(int currentCapacity){
+            this.currentCapacity = currentCapacity;
+            return this;
+        }
+
+        public Ship build(){
+            return new Ship(this);
+        }
+    }
+
+    @Override
+    public void run() {//TODO:implement
+
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Ship[");
+        sb.append("id=").append(id);
+        sb.append(", currentCapacity=").append(currentCapacity);
+        sb.append(']');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ship)) return false;
+        Ship ship = (Ship) o;
+        return id == ship.id &&
+                totalCapacity == ship.totalCapacity &&
+                currentCapacity == ship.currentCapacity;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, totalCapacity, currentCapacity);
+    }
 }
