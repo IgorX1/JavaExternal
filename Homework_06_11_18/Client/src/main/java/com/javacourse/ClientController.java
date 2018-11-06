64d3fd72239ee7ab16f210111b9d0c10a2f91c99
Igor Konobas <kois9911@gmail.com>
2018-11-06 16:36:23 +0200

    Implemented client without plot visualization and withod any testing

--- Homework_06_11_18/Client/src/main/java/com/javacourse/ClientController.java
+++ Homework_06_11_18/Client/src/main/java/com/javacourse/ClientController.java
@@ -1,9 +1,10 @@
 package com.javacourse;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
@@ -28,23 +29,32 @@ public class ClientController {
 
     public void processUser(){
         view.showMessage("Welcome to RPN calculator!");
-
+        String expression, result;
+        PrintWriter out;
+        BufferedReader in;
         try {
             initSocket();
+            out = initOutputStream();
+            in = initInputStream();
+            do{
+                expression = getExpression();
+                result = processExpressionOnServer(expression, out, in);
+                showResult(result);
+            }while (true);
         } catch (IOException e) {
             view.showMessage("Unable to establish the connection.");
             return;
+        }finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                logger.error(e.getMessage());
+            }
         }
 
-        String expression;
-        do{
-            expression = getExpression();
-            processExpressionOnServer(expression);
-
-        }while (true);
     }
 
-    String getExpression(){
+    String getExpression(){//TODO:  implement exiting from infinite loop by typing some phrase
         view.showMessage("Enter the expression to calculate");
         view.showMessage("Allowed operations:+ - / * sin lg ()");
         String expr = scanner.nextLine();
@@ -61,7 +71,39 @@ public class ClientController {
         }
     }
 
-    private void processExpressionOnServer(String expression) {
+    PrintWriter initOutputStream() throws IOException{
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+                socket.getOutputStream()
+        )), true);
+        return out;
+    }
+
+    BufferedReader initInputStream() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                socket.getInputStream()
+        ));
+        return in;
+    }
+
+    String processExpressionOnServer(String expression, PrintWriter out, BufferedReader in) throws IOException {
+        out.print(expression);
+        return readFileFromServer(in);
+    }
+
+    String readFileFromServer(BufferedReader in) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while((line = in.readLine())!=null){
+            sb.append(line);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Will parse the resulting string and, in the long run, will show the plot
+     * @param result is an XML-file which is to be parsed in helper methods
+     */
+    void showResult(String result){
 
     }
 }
