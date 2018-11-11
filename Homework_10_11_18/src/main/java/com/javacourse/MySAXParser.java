dcdc3898b550c8502493c38a351b6ae4e237328a
Igor Konobas <kois9911@gmail.com>
2018-11-11 17:40:05 +0200

    Implemented SAX validation

--- Homework_10_11_18/src/main/java/com/javacourse/MySAXParser.java
+++ Homework_10_11_18/src/main/java/com/javacourse/MySAXParser.java
@@ -2,9 +2,11 @@ package com.javacourse;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import javax.xml.validation.SchemaFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -20,6 +22,10 @@ public class MySAXParser extends DefaultHandler implements XMLParser {
         List<Page> pageEntitiesList = null;
         try {
             SAXParser parser = spf.newSAXParser();
+            spf.setValidating(true);
+            spf.setNamespaceAware(true);
+            SchemaFactory sFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            spf.setSchema(sFactory.newSchema(new File(schemaPath)));
             parser.parse(new File(pathToXmlFile), handler);
             pageEntitiesList = handler.getPageEntitiesList();
         } catch (ParserConfigurationException | SAXException | IOException e) {
