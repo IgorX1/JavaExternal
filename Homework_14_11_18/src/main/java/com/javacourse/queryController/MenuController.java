70dc38a96fab584e7cb2045e016817b4d1d3ca7a
Igor Konobas <kois@ukr.net>
2018-11-18 11:43:14 +0200

    Working on implementing Command GOF pattern

--- Homework_14_11_18/src/main/java/com/javacourse/queryController/MenuController.java
+++ Homework_14_11_18/src/main/java/com/javacourse/queryController/MenuController.java
@@ -1,11 +1,22 @@
 package com.javacourse.queryController;
 
 public class MenuController {
-    Command[] onCommands;
-    Command[] offCommands;
-    Command undoCommand;
+    private static final int COMMAND_COUNT = 5;
+    private Command[] commands;
 
     public MenuController() {
+        commands = new Command[COMMAND_COUNT];
+        Command noCommand = new NoCommand();
+        for(int slot=0; slot<COMMAND_COUNT; ++slot){
+            commands[slot] = noCommand;
+        }
+    }
 
+    public void setCommand(int slot, Command cmd){
+        commands[slot] = cmd;
+    }
+
+    public void onMenuItemWasPickedUp(int slot){
+        commands[slot].execute();
     }
 }
---
+++ Homework_14_11_18/src/main/java/com/javacourse/queryController/NoCommand.java
@@ -0,0 +1,8 @@
+package com.javacourse.queryController;
+
+public class NoCommand implements Command {
+    @Override
+    public void execute() {
+
+    }
+}
