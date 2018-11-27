d82994d3c28d45b728eeb5e35c37984ba33a10fb
Igor Konobas <kois@ukr.net>
2018-11-27 20:18:13 +0200

    Implemented role safety

--- Homework_24_11_18/src/main/java/com/javacourse/servlet/filter/AdminAccessFilter.java
+++ Homework_24_11_18/src/main/java/com/javacourse/servlet/filter/AdminAccessFilter.java
@@ -1,11 +1,31 @@
 package com.javacourse.servlet.filter;
 
+import com.javacourse.model.User;
+
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@WebFilter(urlPatterns = {"/pages/adminview/*"})
 public class AdminAccessFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession(false);
+
+        if(isLoggedIn(session)){
+            User.ROLE role = (User.ROLE) session.getAttribute("role");
+            if(role != User.ROLE.ADMIN){
+                response.sendRedirect(request.getContextPath() + "/pages/shared/access_denied.jsp");
+            }else filterChain.doFilter(servletRequest, servletResponse);
+        }else {
+            //case when user is not authorized
+            response.sendRedirect(request.getContextPath() + "/pages/login.jsp");
+        }
 
     }
 
@@ -17,5 +37,11 @@ public class AdminAccessFilter implements Filter {
     @Override
     public void destroy() {
 
+    }
+
+    boolean isLoggedIn(HttpSession session){
+        return (session!=null
+                && session.getAttribute("login")!=null
+                && session.getAttribute("password")!=null);
     }
 }
--- Homework_24_11_18/src/main/webapp/pages/shared/access_denied.jsp
+++ Homework_24_11_18/src/main/webapp/pages/shared/access_denied.jsp
@@ -12,6 +12,6 @@
 </head>
 <body>
 <h1 style="color:red;">Access denied!</h1>
-<p><a href="${pageContext.request.contextPath}/index.jsp">HOME PAGE</a></p>
+<p><a href="${pageContext.request.contextPath}/index.jsp">LOG IN WITH OTHER ACCOUNT</a></p>
 </body>
 </html>
